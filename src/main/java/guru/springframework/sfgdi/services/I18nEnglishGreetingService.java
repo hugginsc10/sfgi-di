786aabf589f944151ad2035afbd617c1ba9019bd
Chas Huggins <hugginsc10@gmail.com>
2022-11-04 21:58:40 -0400

    added DI to greetings service config

--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -1,7 +1,6 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
 
 /**
  * Created by jt on 12/27/19.
@@ -9,6 +8,14 @@ import org.springframework.stereotype.Service;
 
 
 public class I18nEnglishGreetingService implements GreetingService {
+    
+    private final EnglishGreetingRepository englishGreetingRepository;
+
+    public I18nEnglishGreetingService(EnglishGreetingRepository englishGreetingRepository) {
+        this.englishGreetingRepository = englishGreetingRepository;
+    }
+
+
     @Override
     public String sayGreeting() {
         return "Hello World - EN";
